34955698592aa09e211a919ce88be225ebe9cac2
daniel david kovacs <kicsikrumpli@gmail.com>
2015-03-04 22:46:44 +0100

    fixed home dir resolver

--- src/main/java/io/github/kicsikrumpli/dao/HomeDirectoryResolver.java
+++ src/main/java/io/github/kicsikrumpli/dao/HomeDirectoryResolver.java
@@ -2,6 +2,8 @@ package io.github.kicsikrumpli.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * Replaces ~ in path with home directory.
  * @author daniel
@@ -9,19 +11,27 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class HomeDirectoryResolver {
-    
-    /**
+    private static final String USER_HOME = "user.home";
+	private static final String EMPTY = "";
+	private static final String BEGINS_WITH_TILDE = "^~";
+
+	/**
      * Resolves home directory from user.home system property;
-     * @param path
-     * @return
+     * @param path with home to be resolved
+     * @return path with resolved home
      */
     public String resolveHome(String path) {
         String home;
         try {
-            home = System.getProperty("user.home");
+            home = getUserHome();
         } catch(IllegalArgumentException e) {
-            home = "";
+            home = EMPTY;
         }
-        return path.replaceFirst("~", home );
+        return path.replaceFirst(BEGINS_WITH_TILDE, home);
     }
+
+    @VisibleForTesting
+	String getUserHome() {
+		return System.getProperty(USER_HOME);
+	}
 }
---
+++ src/test/java/io/github/kicsikrumpli/dao/HomeDirectoryResolverTest.java
@@ -0,0 +1,63 @@
+package io.github.kicsikrumpli.dao;
+
+import static org.mockito.BDDMockito.given;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+/**
+ * Test class for {@link HomeDirectoryResolver}.
+ * @author kicsikrumpli1
+ *
+ */
+public class HomeDirectoryResolverTest {
+	@Spy
+	@InjectMocks
+	private HomeDirectoryResolver underTest;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testResolveShouldRemoveLeadingTildeWhenUserHomeSystemPropertyNotSet() {
+		// GIVEN
+		given(underTest.getUserHome()).willThrow(new IllegalArgumentException());
+		
+		// WHEN
+		String resolvedPath = underTest.resolveHome("~dummy-path");
+		
+		// THEN
+		MatcherAssert.assertThat(resolvedPath, Matchers.is("dummy-path"));
+	}
+	
+	@Test
+	public void testResolveShouldReplaceLeadingTildeWhenUserHomeSystemPropertySet() {
+		// GIVEN
+		given(underTest.getUserHome()).willReturn("/user/home");
+		
+		// WHEN
+		String resolvedPath = underTest.resolveHome("~/dummy-path");
+		
+		// THEN
+		MatcherAssert.assertThat(resolvedPath, Matchers.is("/user/home/dummy-path"));
+	}
+	
+	@Test
+	public void testResolveShouldLeaveNonLeadingTildeUnchanged() {
+		// GIVEN
+		given(underTest.getUserHome()).willReturn("/user/home");
+		
+		// WHEN
+		String resolvedPath = underTest.resolveHome("/dummy~path");
+		
+		// THEN
+		MatcherAssert.assertThat(resolvedPath, Matchers.is("/dummy~path"));
+	}
+}
