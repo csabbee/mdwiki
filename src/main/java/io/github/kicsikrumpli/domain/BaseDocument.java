3957d4b505f7bd114512f3ba88f2183c48fc9e40
Daniel David Kovacs <kicsikrumpli1@mr-bunnys-mbp.local>
2015-03-04 22:12:50 +0100

    added equals

--- src/main/java/io/github/kicsikrumpli/domain/BaseDocument.java
+++ src/main/java/io/github/kicsikrumpli/domain/BaseDocument.java
@@ -21,5 +21,35 @@ public abstract class BaseDocument {
     protected void setAuthor(String author) {
         this.author = author;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseDocument other = (BaseDocument) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
    
 }
--- src/main/java/io/github/kicsikrumpli/domain/MarkdownDocument.java
+++ src/main/java/io/github/kicsikrumpli/domain/MarkdownDocument.java
@@ -42,4 +42,29 @@ public class MarkdownDocument extends BaseDocument {
 	        return new MarkdownDocument(this);
 	    }
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarkdownDocument other = (MarkdownDocument) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		return true;
+	}
 }
