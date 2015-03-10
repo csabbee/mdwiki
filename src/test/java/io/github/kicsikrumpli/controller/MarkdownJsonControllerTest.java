8c9e68f9adccb551d09a601504478c2594fe2ccc
daniel david kovacs <kicsikrumpli@gmail.com>
2015-03-10 18:45:50 +0100

    added UT to json controller

--- src/test/java/io/github/kicsikrumpli/controller/MarkdownJsonControllerTest.java
+++ src/test/java/io/github/kicsikrumpli/controller/MarkdownJsonControllerTest.java
@@ -1,16 +1,21 @@
 package io.github.kicsikrumpli.controller;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 import io.github.kicsikrumpli.controller.converter.DocumentStoreRequestConverter;
 import io.github.kicsikrumpli.service.DocumentStore;
 import io.github.kicsikrumpli.service.domain.DocumentStoreRequest;
 import io.github.kicsikrumpli.service.domain.MarkdownDocument;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import com.google.common.base.Optional;
 
 /**
  * Test class for {@link MarkdownJsonController}.
@@ -26,31 +31,61 @@ public class MarkdownJsonControllerTest {
     @Mock
     private DocumentStoreRequestConverter mockDocumentRequestConverter;
     private String mockDocumentName;
-    private DocumentStoreRequest mockDocumentStoreRequest = createMockDocumentStoreRequest();
+    private DocumentStoreRequest mockDocumentStoreRequest;
+	private MarkdownDocument mockMarkdownDocument;
 
-    private DocumentStoreRequest createMockDocumentStoreRequest() {
-        return new DocumentStoreRequest.Builder().withDocumentName(MOCK_DOCUMENT_NAME).build();
-    }
-    
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         mockDocumentName = MOCK_DOCUMENT_NAME;
+        mockDocumentStoreRequest = createMockDocumentStoreRequest();
+        mockMarkdownDocument = createMockMarkdownDocument();
     }
-    
-    // testGetMarkdownDocumentShouldConvertDocumentNameToRequestObject()
-    // testGetMarkdownDocumentShouldReturnDocumentWhenFileIsPresent()
-    // testGetMarkdownDocumentShouldThrowDocumentNotFoundExceptionWhenFileIsNotPresent()
-    
+
     @Test
     public void testGetMarkdownDocumentShouldConvertDocumentNameToRequestObject() {
         // GIVEN
-        BDDMockito.given(mockDocumentRequestConverter.convert(mockDocumentName)).willReturn(mockDocumentStoreRequest);
+        given(mockDocumentRequestConverter.convert(mockDocumentName)).willReturn(mockDocumentStoreRequest);
+        given(mockDocumentStore.retrieveDocument(mockDocumentStoreRequest)).willReturn(Optional.of(mockMarkdownDocument));
         
         // WHEN
-        MarkdownDocument response = underTest.getMarkdownDocument(mockDocumentName);
+        underTest.getMarkdownDocument(mockDocumentName);
         
         // THEN
+        Mockito.verify(mockDocumentRequestConverter).convert(mockDocumentName);
+    }
+    
+    @Test(expected = DocumentNotFoundException.class)
+    public void testGetMarkdownDocumentShouldThrowDocumentNotFoundExceptionWhenFileIsNotPresent() {
+        // GIVEN
+        given(mockDocumentRequestConverter.convert(mockDocumentName)).willReturn(mockDocumentStoreRequest);
+        given(mockDocumentStore.retrieveDocument(mockDocumentStoreRequest)).willReturn(Optional.<MarkdownDocument>absent());
+        
+        // WHEN
+        underTest.getMarkdownDocument(mockDocumentName);
+        
+        // THEN
+        Mockito.verify(mockDocumentRequestConverter).convert(mockDocumentName);
+    }
+    
+    @Test
+    public void testGetMarkdownDocumentShouldReturnDocumentWhenFileIsPresent() {
+        // GIVEN
+        given(mockDocumentRequestConverter.convert(mockDocumentName)).willReturn(mockDocumentStoreRequest);
+        given(mockDocumentStore.retrieveDocument(mockDocumentStoreRequest)).willReturn(Optional.of(mockMarkdownDocument));
+        
+        // WHEN
+        MarkdownDocument request = underTest.getMarkdownDocument(mockDocumentName);
+        
+        // THEN
+        assertThat(request, Matchers.is(mockMarkdownDocument));
     }
 
+	private MarkdownDocument createMockMarkdownDocument() {
+		return new MarkdownDocument.Builder().build();
+	}
+
+    private DocumentStoreRequest createMockDocumentStoreRequest() {
+        return new DocumentStoreRequest.Builder().withDocumentName(MOCK_DOCUMENT_NAME).build();
+    }
 }
