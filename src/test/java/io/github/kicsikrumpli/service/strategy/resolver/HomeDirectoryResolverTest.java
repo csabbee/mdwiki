d97740e5522ec5dddb92c5a770f8d2f34ccef518
Daniel David Kovacs <kicsikrumpli@gmail.com>
2015-03-10 13:23:45 +0100

    added home directory resolver test

--- src/test/java/io/github/kicsikrumpli/service/strategy/resolver/HomeDirectoryResolverTest.java
+++ src/test/java/io/github/kicsikrumpli/service/strategy/resolver/HomeDirectoryResolverTest.java
@@ -1,5 +1,64 @@
 package io.github.kicsikrumpli.service.strategy.resolver;
 
-public class HomeDirectoryResolverTest {
+import static org.mockito.BDDMockito.given;
+import io.github.kicsikrumpli.service.strategy.resolver.HomeDirectoryResolver;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+/**
+ * Test class for {@link HomeDirectoryResolver}.
+ * @author kicsikrumpli1
+ *
+ */
+public class HomeDirectoryResolverTest {
+    @Spy
+    @InjectMocks
+    private HomeDirectoryResolver underTest;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void testResolveShouldRemoveLeadingTildeWhenUserHomeSystemPropertyNotSet() {
+        // GIVEN
+        given(underTest.getUserHome()).willThrow(new IllegalArgumentException());
+        
+        // WHEN
+        String resolvedPath = underTest.resolveHome("~dummy-path");
+        
+        // THEN
+        MatcherAssert.assertThat(resolvedPath, Matchers.is("dummy-path"));
+    }
+    
+    @Test
+    public void testResolveShouldReplaceLeadingTildeWhenUserHomeSystemPropertySet() {
+        // GIVEN
+        given(underTest.getUserHome()).willReturn("/user/home");
+        
+        // WHEN
+        String resolvedPath = underTest.resolveHome("~/dummy-path");
+        
+        // THEN
+        MatcherAssert.assertThat(resolvedPath, Matchers.is("/user/home/dummy-path"));
+    }
+    
+    @Test
+    public void testResolveShouldLeaveNonLeadingTildeUnchanged() {
+        // GIVEN
+        given(underTest.getUserHome()).willReturn("/user/home");
+        
+        // WHEN
+        String resolvedPath = underTest.resolveHome("/dummy~path");
+        
+        // THEN
+        MatcherAssert.assertThat(resolvedPath, Matchers.is("/dummy~path"));
+    }
 }
